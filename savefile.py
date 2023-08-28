a79969e511935d30bd16faea85252f24e002d298
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2023-08-28 18:52:33 +0200

    Initial decryption

--- savefile.py
+++ savefile.py
@@ -1,11 +1,32 @@
+from Crypto.Cipher import Blowfish
+
+def endianness_reversal ( data: bytearray ) :
+    for i in range(0, len(data), 4):
+        data[i:i+4] = data[i:i+4][::-1]
+    return data
+def hexdump ( data: bytearray, start: int = 0, end: int = None, width: int = 16 ) :
+    if end is None :
+        end = len(data)
+    for i in range(start , end , width) :
+        ascii = ""
+        for j in range(i, i+width) :
+            if j < end :
+                ascii += chr(data[j]) if 32 <= data[j] <= 126 else "."
+            else :
+                ascii += " "
+        hexstr = " ".join(f"{data[j]:02x}" for j in range(i, i+width) if j < end)
+        print(f"{i:08x}  {hexstr:48}  {ascii}")
 
 class Savefile :
+    # Monster Hunter World savefile class
+
+    cipher = Blowfish.new(b"xieZjoe#P2134-3zmaghgpqoe0z8$3azeq", Blowfish.MODE_ECB)
 
     def __init__ ( self ) :
         pass
 
     def read_savefile ( self , filename ) :
-        print(f"Reading savefile {filename}...")
         with open(filename , "rb") as f :
-            self.savefile = f.read()
-        print(f"Read {len(self.savefile)} bytes.")
+            data = bytearray(f.read())
+        self.savefile = bytearray(endianness_reversal(bytearray(Savefile.cipher.decrypt(endianness_reversal(data)))))
+        hexdump(self.savefile, 0, 0x100)
