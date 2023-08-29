88f30f94e1d71091c65d9ca6f6efb6e1b91e929d
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2023-08-29 14:17:00 +0200

    Not yet working character decryption

--- savefile.py
+++ savefile.py
@@ -1,9 +1,5 @@
-from Crypto.Cipher import Blowfish
+from mhwcrypto import MHWCrypto
 
-def endianness_reversal ( data: bytearray ) :
-    for i in range(0, len(data), 4):
-        data[i:i+4] = data[i:i+4][::-1]
-    return data
 def hexdump ( data: bytearray, start: int = 0, end: int = None, width: int = 16 ) :
     if end is None :
         end = len(data)
@@ -14,13 +10,11 @@ def hexdump ( data: bytearray, start: int = 0, end: int = None, width: int = 16 
                 ascii += chr(data[j]) if 32 <= data[j] <= 126 else "."
             else :
                 ascii += " "
-        hexstr = " ".join(f"{data[j]:02x}" for j in range(i, i+width) if j < end)
-        print(f"{i:08x}  {hexstr:48}  {ascii}")
+        hexstr = " ".join(f"{data[j]:02X}" for j in range(i, i+width) if j < end).replace("0", "-")
+        print(f"{i:08X}  {hexstr:48}  {ascii}")
 
 class Savefile :
     # Monster Hunter World savefile class
-
-    cipher = Blowfish.new(b"xieZjoe#P2134-3zmaghgpqoe0z8$3azeq", Blowfish.MODE_ECB)
 
     def __init__ ( self ) :
         pass
@@ -28,7 +22,8 @@ class Savefile :
     def read_savefile ( self , filename ) :
         with open(filename , "rb") as f :
             data = bytearray(f.read())
-        self.savefile = bytearray(endianness_reversal(bytearray(Savefile.cipher.decrypt(endianness_reversal(data)))))
+        self.savefile = MHWCrypto().decrypt_savefile(data)
+
         self.infodump()
 
     def infodump ( self ) :
@@ -42,7 +37,7 @@ class Savefile :
         print(f"OffsetControls          : [0x000040 - 0x000048] = 000060")
         print(f"OffsetOptions           : [0x000048 - 0x000050] = 300070")
         print(f"OffsetSection2          : [0x000050 - 0x000058] = 301080")
-        print(f"OffsetCharacters        : [0x000058 - 0x000060] = 3010C8 {int.from_bytes(self.savefile[0x000058:0x000060], byteorder='little'):08X}")
+        print(f"OffsetCharacters        : [0x000058 - 0x000060] = 3010C8")
         # Controls
         print(f"ControlsSignature       : [0x000060 - 0x000064] = {self.savefile[0x000060:0x000064].hex().upper()}")
         print(f"UNKNOWN_64_4            : [0x000064 - 0x000068] = 0C 00 00 00")
@@ -63,5 +58,12 @@ class Savefile :
         print(f"UNKNOWN_3010CC_4        : [0x3010CC - 0x3010D0] = 0C 00 00 00")
         print(f"CharactersSize          : [0x3010D0 - 0x3010D8] = 7C2000")
         print(f"CharactersData          : [0x3010D8 - 0xAC30D8]")
+        print(f"- Character1            : [0x3010D8 - 0x50AB98]")
+        hexdump(self.savefile, 0x3010D8, 0x3010D8+0x100) # 0x2098C0 0x209AC0
+        print(f"- Character2            : [0x50AB98 - 0x714658]")
+        hexdump(self.savefile, 0x50AB98, 0x50AB98+0x100) # 0x2098C0 0x209AC0
+        print(f"- Character3            : [0x714658 - 0xAC30D8]")
+        hexdump(self.savefile, 0x714658, 0x714658+0x100) # 0x2098C0 0x209AC0
+        print(f"- PADDING_91E118_1A4FC0 : [0x91E118 - 0xAC30D8]")
         # Padding
         print(f"PADDING_AC30D8_8        : [0xAC30D8 - 0xAC30E0]")
